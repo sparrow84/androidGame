d7974a1cb15c76637418a1bb5d8fb8928c165f9b
sparrow84 <vorobey280184@mail.ru>
2018-10-20 01:21:12 +0700

    Start

---
+++ core/src/com/github/sparrow84/jagd/StarGame.java
@@ -0,0 +1,43 @@
+package com.github.sparrow84.jagd;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture badLogic;
+	Texture funnyCat;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		badLogic = new Texture("badlogic.jpg");
+		funnyCat = new Texture("qwe.png");
+		region = new TextureRegion(badLogic, 144, 20, 50, 30);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+//		batch.draw(funnyCat, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(funnyCat, 0, 0, 200, 200);
+//		batch.setColor(1, 0, 0, 1);
+//		batch.draw(badLogic, 100, 100);
+//		batch.setColor(0, 1, 0, 1);
+//		batch.draw(region, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		badLogic.dispose();
+	}
+}
---
+++ desktop/src/com/github/sparrow84/jagd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.sparrow84.jagd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.sparrow84.jagd.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
