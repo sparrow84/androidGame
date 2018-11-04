09421c595c15218b2ebfaf8fadae340c0117644c
sparrow84 <vorobey280184@mail.ru>
2018-11-04 18:46:08 +0700

    Lesson05 prepare to lesson 5

--- core/src/com/github/sparrow84/jagd/Star2DGame.java
+++ core/src/com/github/sparrow84/jagd/Star2DGame.java
@@ -7,7 +7,7 @@ import com.github.sparrow84.jagd.screen.MenuScreen;
 public class Star2DGame extends Game {
     @Override
     public void create() {
-        setScreen(new MenuScreen());
+        setScreen(new MenuScreen(this));
 
 
 
